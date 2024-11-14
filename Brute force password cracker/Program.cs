d79612e4658958e0885f65dfedeb771e61f9f796
Dan <66948931+Danicus2000000@users.noreply.github.com>
2024-11-14 09:00:01 +0000

    Upgrade brute force password checker to NET 9

--- Brute force password cracker/Program.cs
+++ Brute force password cracker/Program.cs
@@ -1,11 +1,9 @@
-﻿using System;
-using System.Diagnostics;
-using System.IO;
-namespace Brute_force_passowrd_checker
+﻿using System.Diagnostics;
+namespace Brute_force_password_cracker
 {
     class Program
     {
-        public static char[] characters=new char[] { 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',' ', '!', '\"', '#', '$', '%', '&', '\'', '(', ')', '*', '+', ',', '-', '.', '/', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', ':', ';', '<', '=', '>', '?', '@', '[', '\\', ']', '^', '_', '`', '{', '|', '}', '~' };
+        public static char[] characters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', ' ', '!', '\"', '#', '$', '%', '&', '\'', '(', ')', '*', '+', ',', '-', '.', '/', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', ':', ';', '<', '=', '>', '?', '@', '[', '\\', ']', '^', '_', '`', '{', '|', '}', '~'];
         public static int passwordLength = 6;
         public static long tries = 0;
         public static bool done = false;
@@ -20,7 +18,7 @@ namespace Brute_force_passowrd_checker
             {
                 return;
             }
-            for (char c = characters[0]; c <= characters[characters.Length-1]; c++)
+            for (char c = characters[0]; c <= characters[^1]; c++)
             {
                 if (done != true)
                 {
@@ -46,7 +44,7 @@ namespace Brute_force_passowrd_checker
                 Console.WriteLine(".");
                 Console.ForegroundColor = ConsoleColor.DarkGreen;
                 Console.Write("\nPlease enter your password > ");
-                password = Convert.ToString(Console.ReadLine()).ToLower();
+                password = Convert.ToString(Console.ReadLine() ?? "").ToLower();
                 Console.ForegroundColor = ConsoleColor.DarkRed;
                 Console.WriteLine("\nCracking your password...");
                 Stopwatch timer = Stopwatch.StartNew();
@@ -59,7 +57,7 @@ namespace Brute_force_passowrd_checker
                 {
                     Console.WriteLine("\n\nYour password has been found! Here are the statistics:");
                     Console.WriteLine(")----------------------------------------------------(");
-                    Console.WriteLine("Password: {0}",password);
+                    Console.WriteLine("Password: {0}", password);
                     Console.WriteLine("Password length: {0}", passwordLength);
                     Console.WriteLine("Tries: {0}", tries);
                     string plural = "seconds";
@@ -74,16 +72,16 @@ namespace Brute_force_passowrd_checker
                 {
                     Console.WriteLine("\n\nYour password has been found! Here are the statistics:");
                     Console.WriteLine(")----------------------------------------------------(");
-                    Console.WriteLine("Password: {0}",password);
-                    Console.WriteLine("Password length: {0}",passwordLength);
-                    Console.WriteLine("Tries: {0}",tries);
+                    Console.WriteLine("Password: {0}", password);
+                    Console.WriteLine("Password length: {0}", passwordLength);
+                    Console.WriteLine("Tries: {0}", tries);
                     Console.WriteLine("Time to crack: {0} seconds", elapsedTime);
                 }
                 Console.ForegroundColor = ConsoleColor.DarkYellow;
                 Console.Write("\n\nWould you like to go again? (y/n) ");
-                string result = Console.ReadLine();
+                string result = Console.ReadLine() ?? "";
                 result = result.ToUpper();
-                if (result=="N" || result=="NO" || result=="NAH" || result=="NOPE")
+                if (result == "N" || result == "NO" || result == "NAH" || result == "NOPE")
                 {
                     break;
                 }
